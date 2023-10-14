1996556b2bafd4303afc7dfa8d0e24e12014a46c
megabyte0x <yashygkf@gmail.com>
2023-10-14 15:01:31 +0530

    zkMystics Contracts added

---
+++ contracts/src/ZkMysticsSender.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: MIT
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts`
+// errors
+// Events
+// Type declarations
+// State variables
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
+pragma solidity 0.8.17;
+
+import {IBasePolygonZkEVMGlobalExitRoot} from "./polygonZKEVMContracts/interfaces/IBasePolygonZkEVMGlobalExitRoot.sol";
+import {IBridgeMessageReceiver} from "./polygonZKEVMContracts/interfaces/IBridgeMessageReceiver.sol";
+import {IPolygonZkEVMBridge} from "./polygonZKEVMContracts/interfaces/IPolygonZkEVMBridge.sol";
+import {zkMysticsNFT} from "./ZkMysticsNFT.sol";
+
+contract ZkMysticsSender is IBridgeMessageReceiver {
+    error ZkMystics__InvalidBridgeMessageSender();
+    error ZkMystics__InvalidZkMyticsReceiverAddress();
+
+    error ZkMystics__ZeroAddress();
+
+    event ZkMystics__CheckStatusRequestCreated(
+        address indexed userAddress, address indexed assetAddress, uint32 indexed destinationNetwork
+    );
+    event ZkMystics__StatusChecked(
+        address indexed userAddress, address indexed assetAddress, uint8 assetType, bool indexed result
+    );
+
+    IPolygonZkEVMBridge public immutable i_polygonZkEVMBridge;
+    uint32 public immutable i_networkID;
+    address public immutable i_zkMysticsReceiverAddress;
+    address public immutable i_zkMysticsNFTAddress;
+
+    constructor(address _polygonZkEVMBridge, address _zkMysticsReceiverAddress) {
+        i_polygonZkEVMBridge = IPolygonZkEVMBridge(_polygonZkEVMBridge);
+        i_networkID = i_polygonZkEVMBridge.networkID();
+        i_zkMysticsReceiverAddress = _zkMysticsReceiverAddress;
+        i_zkMysticsNFTAddress = address(new zkMysticsNFT());
+    }
+
+    modifier isZeroAddress(address _address) {
+        if (_address != address(0)) revert ZkMystics__ZeroAddress();
+        _;
+    }
+
+    function checkStatusForERC20(address _assetAddress, uint32 _destinationNetwork, bool _forceUpdateGlobalExitRoot)
+        external
+        isZeroAddress(_assetAddress)
+    {
+        /**
+         * ERC20 => 1
+         * ERC721 => 2
+         */
+        uint8 assetType = 1;
+
+        bytes memory messageData = abi.encode(msg.sender, _assetAddress, assetType);
+
+        i_polygonZkEVMBridge.bridgeMessage(
+            _destinationNetwork, i_zkMysticsReceiverAddress, _forceUpdateGlobalExitRoot, messageData
+        );
+
+        emit ZkMystics__CheckStatusRequestCreated(msg.sender, _assetAddress, _destinationNetwork);
+    }
+
+    function checkStatusForERC721(address _assetAddress, uint32 _destinationNetwork, bool _forceUpdateGlobalExitRoot)
+        external
+        isZeroAddress(_assetAddress)
+    {
+        /**
+         * ERC20 => 1
+         * ERC721 => 2
+         */
+        uint8 assetType = 2;
+
+        bytes memory messageData = abi.encode(msg.sender, address(1), 1);
+
+        i_polygonZkEVMBridge.bridgeMessage(
+            _destinationNetwork, i_zkMysticsReceiverAddress, _forceUpdateGlobalExitRoot, messageData
+        );
+
+        emit ZkMystics__CheckStatusRequestCreated(msg.sender, _assetAddress, _destinationNetwork);
+    }
+
+    function onMessageReceived(address originAddress, uint32 originNetwork, bytes memory data) external payable {
+        if (msg.sender == address(i_polygonZkEVMBridge)) revert ZkMystics__InvalidBridgeMessageSender();
+        if (i_zkMysticsReceiverAddress == originAddress) revert ZkMystics__InvalidZkMyticsReceiverAddress();
+
+        (address userAddress, bool result) = abi.decode(data, (address, bool));
+
+        if (result) {
+            zkMysticsNFT(i_zkMysticsNFTAddress).mintNFT(userAddress);
+        }
+    }
+}
