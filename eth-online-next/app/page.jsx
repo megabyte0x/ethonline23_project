1d52a4fef11ffec60368a762e49e0b6b7d65e2a3
L-Pircy <taveesha.agarwal@gmail.com>
2023-10-17 20:00:12 +0530

    dynamic wallet claims ape lens verification

--- eth-online-next/app/page.jsx
+++ eth-online-next/app/page.jsx
@@ -13,18 +13,70 @@ export default function Home() {
     <>
       <main className="rounded-[33px] h-[100%] w-[100%] bg-[#ffffff] p-9 grid gap-6 bgSh">
         <AboutUs />
-        <div className="grid grid-cols-3 gap-6">
-          <div className="col-span-2 grid gap-6 h-fit">
-            <ClaimList />
-            <YourNFTs />
+        {walletAdd && (
+          <div className="grid grid-cols-3 gap-6">
+            <div className="col-span-2 grid gap-6 h-fit">
+              {apeCoin.stat && (
+                <ClaimList listType="APE Coin" list={apeCoin.claimList} />
+              )}
+              {lensNFT.stat && (
+                <ClaimList listType="Lens NFT" list={lensNFT.claimList} />
+              )}
+              <YourNFTs />
+            </div>
+
+            <div className="col-span-1 grid gap-6 h-fit">
+              <ConnectWallet
+                walletAdd={walletAdd}
+                ape={apeCoin.stat}
+                lens={lensNFT.stat}
+              />
+              <Minted />
+            </div>
           </div>
-          <div className="col-span-1 grid gap-6 h-fit">
-            <ConnectWallet />
-            <Minted />
-          </div>
-        </div>
+        )}
+        {!walletAdd && <ConnectWallet walletAdd={walletAdd} />}
       </main>
       <MintModal />
     </>
   );
 }
+
+const walletAdd = "0x27923CAB90564c5C195BbFb98f7DA8d3D4F751Fb";
+// const walletAdd = "";
+
+const apeCoin = {
+  stat: true,
+  claimList: [
+    {
+      hash: "0x27923CAB90564c5C19",
+      claimStat: true,
+    },
+    {
+      hash: "0x27923CAB90564c5C19",
+      claimStat: false,
+    },
+    {
+      hash: "0x27923CAB90564c5C19",
+      claimStat: false,
+    },
+  ],
+};
+
+const lensNFT = {
+  stat: true,
+  claimList: [
+    {
+      hash: "0x27923CAB90564c5C19",
+      claimStat: true,
+    },
+    {
+      hash: "0x27923CAB90564c5C19",
+      claimStat: false,
+    },
+    {
+      hash: "0x27923CAB90564c5C19",
+      claimStat: false,
+    },
+  ],
+};
