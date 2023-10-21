76f5cfaba9a343d823492b2b60fcf53908839005
megabyte0x <yashygkf@gmail.com>
2023-10-21 20:27:03 +0530

    removed the unwanted params

--- subgraph/src/zk-mystic-receiver.ts
+++ subgraph/src/zk-mystic-receiver.ts
@@ -7,23 +7,6 @@ import {
   ZkMystics__StatusChecked
 } from "../generated/schema"
 
-export function handleZkMystics__CheckStatusRequestCreated(
-  event: ZkMystics__CheckStatusRequestCreatedEvent
-): void {
-  let entity = new ZkMystics__CheckStatusRequestCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.userAddress = event.params.userAddress
-  entity.assetAddress = event.params.assetAddress
-  entity.destinationNetwork = event.params.destinationNetwork
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
 export function handleZkMystics__StatusChecked(
   event: ZkMystics__StatusCheckedEvent
 ): void {
@@ -32,12 +15,7 @@ export function handleZkMystics__StatusChecked(
   )
   entity.userAddress = event.params.userAddress
   entity.assetAddress = event.params.assetAddress
-  entity.assetType = event.params.assetType
   entity.result = event.params.result
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
 
   entity.save()
 }
